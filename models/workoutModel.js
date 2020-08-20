392d2072aa2f6d908a75a4665357861ba1ce1652
Jonayah Jackson <jonayah@thinkhalcyon.com>
2020-08-20 19:35:18 -0400

    updated server file

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -32,12 +32,29 @@ const workoutSchema = new Schema(
       // if cardio
       distance: {
         type: Number,
-      }
-    }
-  ]
+      },
+    },
+  ],
+},
+{
+  toJSON: {
+    // we include virtuals because the mongoose doesn't inlcude virtual properties, we would have to specify the inclusion for json data.
+    virtuals: true,
+  },
 }
 );
+// adds a virtual(calculated property) field to the schema
+workoutSchema.virtual("totalWeight").get(function () {
+// adding the total weight of the exercises together
+return this.weight * this.reps * this.sets;
+});
+// adds a virtual( calculated property) field to schema
+workoutSchema.virtual("totalDuration").get(function () {
+// the reduce method executes a reducer function(that you provide) on each element of the array, resulting in single output value.
+return this.exercises.reduce((total, exercise) => {
+  return total + exercise.duration;
+}, 0);
+});
 
 const Workout = mongoose.model("Workout", workoutSchema);
-
 module.exports = Workout;
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan")
+const app = express();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000
 
-const app = express();
-
+app.use(logger("dev"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://Jonayah:5z6qecycT!2c@ds161459.mlab.com:61459/heroku_9rnq4q4p", {
